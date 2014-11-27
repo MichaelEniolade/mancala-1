a767ea7c3e2192c2fc6a3df4f4d04a50028c2322
dac <marco@dacol.me>
2014-11-27 13:43:42 +0100

    game controller finished

--- app/src/main/java/me/dacol/marco/mancala/gameLib/gameController/Game.java
+++ app/src/main/java/me/dacol/marco/mancala/gameLib/gameController/Game.java
@@ -7,23 +7,25 @@ import java.util.Observer;
 import java.util.Random;
 
 import me.dacol.marco.mancala.gameLib.board.Board;
+import me.dacol.marco.mancala.gameLib.gameController.actions.ActivePlayer;
+import me.dacol.marco.mancala.gameLib.gameController.actions.BoardUpdated;
+import me.dacol.marco.mancala.gameLib.gameController.actions.Winner;
 import me.dacol.marco.mancala.gameLib.player.Player;
 
 public class Game implements Observer {
 
-    private List<Player> mPlayers = new ArrayList<Player>();
-    private Board mBoard = null;
-    private TurnContext mTurnContext = null;
+    private List<Player> mPlayers;
+    private Board mBoard;
+    private TurnContext mTurnContext;
+
+    private int mPlayingPlayer;
+    private int mNextPlayer;
+    private int mTurnNumber;
+
+    private boolean mEnded;
 
     // Singleton
     private static Game sInstance = null;
-
-    protected Game() {
-        // Called here because this is where players
-        // and board need to be subscribed as observers
-        // so I need to have this before everything
-        initializeTurnContext();
-    }
 
     public Game getInstance() {
         if (sInstance == null) {
@@ -33,25 +35,52 @@ public class Game implements Observer {
         return sInstance;
     }
 
+    protected Game() {
+        setup();
+
+        // Called here because this is where players
+        // and board need to be subscribed as observers
+        // so I need to have this before everything
+        initializeTurnContext();
+    }
+
+    /**
+     * Run before starting a game, has the job to initialize a new game
+     */
     public void setup() {
-        // Check that the player are added to the game before initialize...
-        if (mPlayers.size() > 2) {
-            // Initialize the board
-            initializeBoard();
-        } else {
-            // TODO die?
-            //throw new Exception("You need at least 2 player");
-        }
+        mPlayingPlayer = 0;
+        mNextPlayer = 0;
+        mTurnNumber = 0;
+        mEnded = false;
+        mPlayers = new ArrayList<Player>();
+        mBoard = null;
+        mTurnContext = null;
     }
 
+    /**
+     * This starts the game loop, it will end when a player won or retreat from the game
+     */
     public void start() {
-        // Notify who is the first player that has to move
-        mTurnContext.post(new ActivePlayerAction(
-                mPlayers.get(chooseStartingPlayer())
-        ));
+        while (!isEnded()) {
+            newTurn();
+        }
+
+        if (isEnded()) {
+            Player winningPlayer = getWinner();
+            announceTheWinner(winningPlayer);
+        } else {
+            // TODO How can i end up here?!
+        }
+     }
+
+    //---> Player methods
+    private void createPlayer() {
+        // player created and added to the game, player can be of two types
+        // Human and AI -> I need a player factory!
     }
 
-    /***
+    /**
+     * Register a new player in the game, and subscribe him to the turnContext object
      *
      * @param player
      */
@@ -60,9 +89,47 @@ public class Game implements Observer {
         mTurnContext.addObserver(player);
     }
 
+    private void turnPlayer() {
+        if (mTurnNumber == 0) {
+            chooseStartingPlayer();
+        } else {
+            updatePlayingPlayer();
+        }
+        updateTurnContext();
+    }
+
+    /***
+     * Select randomly the starting player, it supports only 2 players
+     */
+    private void chooseStartingPlayer() {
+        mPlayingPlayer = new Random().nextInt(mPlayers.size());
+        mNextPlayer = (mPlayers.size() -1) - mPlayingPlayer;
+    }
+
+    private void updateTurnContext() {
+        mTurnContext.push(new ActivePlayer(mPlayers.get(mPlayingPlayer)));
+    }
+
+    /**
+     * Switch the two player, right now it supports only a 2 player game
+     */
+    private void updatePlayingPlayer() {
+        int temp = mPlayingPlayer;
+        mPlayingPlayer = mNextPlayer;
+        mNextPlayer = temp;
+    }
+
+    private Player getWinner() {
+        return mBoard.getWinner();
+    }
+
+    private void announceTheWinner(Player player) {
+        mTurnContext.push(new Winner(player));
+    }
+
+    //---> Turn methods
     private void initializeTurnContext() {
         mTurnContext = TurnContext.getInstance();
-        mTurnContext.initialize();
         mTurnContext.addObserver(this);
     }
 
@@ -72,12 +139,38 @@ public class Game implements Observer {
         mTurnContext.addObserver(mBoard);
     }
 
-    private int chooseStartingPlayer() {
-        return new Random().nextInt(mPlayers.size());
+    private void newTurn() {
+        mTurnContext.initialize();
+
+        if (mTurnNumber == 0) {
+            initializeBoard();
+        }
+
+        turnPlayer();
+        mTurnNumber += 1;
     }
 
+    private void checkForWinner(BoardUpdated boardUpdated) {
+        if (boardUpdated.thereIsAWinner()) {
+            toggleEnd();
+        }
+    }
+
+    private void toggleEnd() {
+        mEnded = !mEnded;
+    }
+
+    private boolean isEnded() {
+        return mEnded;
+    }
+
+    //---> interfaces
     @Override
     public void update(Observable observable, Object data) {
-
+        // qui vanno considerati solo gli ultimi eventi che questo oggetto può maneggiare.
+        if (mTurnContext.peek() instanceof BoardUpdated) {
+            BoardUpdated boardUpdated = (BoardUpdated) mTurnContext.pop();
+            checkForWinner(boardUpdated);
+        }
     }
 }
