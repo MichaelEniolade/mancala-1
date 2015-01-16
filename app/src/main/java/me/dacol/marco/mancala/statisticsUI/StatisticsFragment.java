e925a5c4e7199f170a856ad60e5d931d1473d574
ildac <marco@dacol.me>
2015-01-16 11:05:23 +0100

    new statistics UI

--- app/src/main/java/me/dacol/marco/mancala/statisticsUI/StatisticsFragment.java
+++ app/src/main/java/me/dacol/marco/mancala/statisticsUI/StatisticsFragment.java
@@ -1,35 +1,33 @@
 package me.dacol.marco.mancala.statisticsUI;
 
 import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.database.Cursor;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 import me.dacol.marco.mancala.R;
+import me.dacol.marco.mancala.gameUI.OnFragmentInteractionListener;
+import me.dacol.marco.mancala.statisticsLib.StatisticsCallerObject;
+import me.dacol.marco.mancala.statisticsLib.StatisticsHelper;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link StatisticsFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link StatisticsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StatisticsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class StatisticsFragment extends Fragment implements StatisticsCallerObject {
 
     private OnFragmentInteractionListener mListener;
-
+    private StatisticsHelper mStatisticsHelper;
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -41,10 +39,7 @@ public class StatisticsFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static StatisticsFragment newInstance(String param1, String param2) {
         StatisticsFragment fragment = new StatisticsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -55,31 +50,23 @@ public class StatisticsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
-    }
+        StatisticsHelper mStatisticsHelper = new StatisticsHelper(getActivity());
+        mStatisticsHelper.getLoadAll(this);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        return inflater.inflate(R.layout.fragment_statistics, container, false);
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            mListener = (OnFragmentInteractionListener) activity;
+            mListener = (me.dacol.marco.mancala.gameUI.OnFragmentInteractionListener) activity;
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -92,19 +79,18 @@ public class StatisticsFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    @Override
+    public void postResults(List<Cursor> result) {
+        TextView textView = (TextView) getView().findViewById(R.id.text);
+        int HvCStatisticsCount = result.get(0).getCount();
+        int HvHStatisticsCount = result.get(1).getCount();
+        int GameStatisticsCount = result.get(2).getCount();
+
+        textView.setText(
+                "HvCStatistics: " + HvCStatisticsCount +
+                " HvHStatistics: " + HvHStatisticsCount +
+                " GameStats: " + GameStatisticsCount );
+
     }
 
 }
